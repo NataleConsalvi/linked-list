2b22c4932e7ad434f5dfe72910f98a210df6ca2d
Natale <nataleconsalvi06@gmail.com>
2021-09-07 10:30:56 +0200

    modifica test LinkedList

--- tests/testLinkedList.cpp
+++ tests/testLinkedList.cpp
@@ -66,3 +66,63 @@ TEST_CASE("Verifica stampa (to_string)", "[linked_list]") {
     REQUIRE(s == "u, o, v, a");
 
 }
+
+TEST_CASE("Verifica dimensione linked list (size)", "[linked_list]") {
+    
+    nats::LinkedList* ll = nats::init();
+
+    int a = nats::size(ll);
+
+    REQUIRE(a == 0);
+
+    nats::add(ll, "a");
+    nats::add(ll, "v");
+    nats::add(ll, "o");
+    nats::add(ll, "u");
+
+    int b = nats::size(ll);
+
+    REQUIRE(b == 4);
+
+}
+
+TEST_CASE("Verifica ricerca in linked list (search)", "[linked_list]") {
+    
+    nats::LinkedList* ll = nats::init();
+
+    nats::add(ll, "a");
+    nats::add(ll, "v");
+    nats::add(ll, "o");
+    nats::add(ll, "u");
+
+    bool s = nats::search(ll,"a");
+
+    REQUIRE(s == true);
+
+    bool s1 = nats::search(ll,"e");
+
+    REQUIRE(s1 == false);
+
+}
+
+TEST_CASE("Verifica eliminazione in coda (pop)", "[linked_list]") {
+    
+    nats::LinkedList* ll = nats::init();
+
+    REQUIRE_THROWS_AS( nats::pop(ll), std::invalid_argument);
+
+    nats::add(ll, "a");
+    nats::add(ll, "v");
+    nats::add(ll, "o");
+    nats::add(ll, "u");
+
+    const char* deleted = nats::pop(ll);
+    REQUIRE(deleted == "a");
+
+    const char* deleted2 = nats::pop(ll);
+    REQUIRE(deleted2 == "v");
+
+    string s = nats::to_string(ll);
+    REQUIRE(s == "u, o");
+
+}
